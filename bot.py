38d62eab8ee7c20186667cd4bc902e5c4aab1970
Mergrow <reversivelbatle@gmail.com>
2021-08-14 15:06:57 -0300

    now check if channel1 and channel2
    now checks if the channels argments are valid

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import discord
 import time
+from discord.abc import GuildChannel
+from discord.member import Member
 from ruamel.yaml import YAML
 from discord.ext import commands
 import os
@@ -90,28 +92,34 @@ async def wakeup(ctx, member: discord.Member, channel1, channel2 ):
     if member._user.id == ownerid:
         return await ctx.send('Meu mestre não pode ser acordado! ')
 
+
     channel1 = client.get_channel(int(channel1)) #converte o valor string para inteiro e define o id dos canais do wakup (channel1)
     channel2 = client.get_channel(int(channel2)) #converte o valor string para inteiro e define o id dos canais do wakup (channel2)
+    if channel1 is None:                         #Checa se o argumento channel1 existe e retorna um erro se não existir
+        await ctx.send('Canal.ID1 inválido!')   
+    elif channel2 is None:                      #Checa se o argumento channel2 existe e retorna um erro se não existir
+        await ctx.send('Canal.ID2 inválido!')
+    else:
 
-    channel_return = client.get_channel(member.voice.channel.id) #obtem o canal origem do usuário alvo, então converte para dado tipo ID.
-    await ctx.send(f'{member} ACORDA!!!!')
-    await member.move_to(channel1) #move o usuário entre os carais channel1 e channel2
-    time.sleep(0.1)
-    await member.move_to(channel2)
-    time.sleep(0.1)
-    await member.move_to(channel1)
-    time.sleep(0.1)
-    await member.move_to(channel2)
-    time.sleep(0.1)
-    await member.move_to(channel1)
-    time.sleep(0.1)
-    await member.move_to(channel2)
-    time.sleep(0.1)
-    await member.move_to(channel1)
-    time.sleep(0.1)
-    await member.move_to(channel2)
-    time.sleep(0.1)
-    await member.move_to(channel_return) #volta para o canal origem.
+        channel_return = client.get_channel(member.voice.channel.id) #obtem o canal origem do usuário alvo, então converte para dado tipo ID.
+        await ctx.send(f'{member} ACORDA!!!!')
+        await member.move_to(channel1)                               #move o usuário entre os carais channel1 e channel2
+        time.sleep(0.1)
+        await member.move_to(channel2)
+        time.sleep(0.1)
+        await member.move_to(channel1)
+        time.sleep(0.1)
+        await member.move_to(channel2)
+        time.sleep(0.1)
+        await member.move_to(channel1)
+        time.sleep(0.1)
+        await member.move_to(channel2)
+        time.sleep(0.1)
+        await member.move_to(channel1)
+        time.sleep(0.1)
+        await member.move_to(channel2)
+        time.sleep(0.1)
+        await member.move_to(channel_return) #volta para o canal origem.
 
 
 @wakeup.error #checa se não estão faltando argumentos no comando wakeup
@@ -143,5 +151,4 @@ async def version(message):
     await message.send(f'```Versão: {ver}```')
 
 
-
-client.run(bot_token) #starta o bot usando o token.
+client.run(bot_token)
